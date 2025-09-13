35913b772998391864db2afd114274de18f920fa
Vaibhav Hariani <vaibhav.hariani@gmail.com>
2025-09-12 23:23:34 -0400

    groundwork for jason.

--- Proj1/proj1.py
+++ Proj1/proj1.py
@@ -1,4 +1,15 @@
 import numpy as np
 
 if __name__ == "__main__":
-    pass
+    means=[0,3,10]
+    vars = [np.sqrt(3),2,3]
+    NUM_SAMPLES=100000
+    ## dims means, vars, samples
+    normals = np.random.normal(loc=means, scale=vars, size=(NUM_SAMPLES, len(vars),len(means)))
+    ns = [10,20,50]
+    ps = [0.5,0.4,0.9]
+    binomials = np.random.binomial(n=ns,p=ps, size=(1000, len(ns),len(ps)))
+    for m in range(len(means)):
+        for v in range(len(vars)):
+            print(f"expected Mean {means[m]:.2f}, vars {vars[v]:.2f}, got {np.mean(normals[...,v,m]):.2f}")
+            print(f"Binomial Parameters ")
